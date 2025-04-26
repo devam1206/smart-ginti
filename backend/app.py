c86599e133e865fed0636b719b99a8cf8c54c742
Shaaaark126 <devamdesai126@gmail.com>
2025-04-27 01:03:04 +0530

    Backend Deployment stuff

--- backend/app.py
+++ backend/app.py
@@ -154,5 +154,5 @@ def process_video_endpoint():
         os.remove(file_path)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
