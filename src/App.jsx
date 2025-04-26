9a7c7c7422bbee99f9e67eb3cde9239e2877ebf1
Shaaaark126 <devamdesai126@gmail.com>
2025-04-27 01:31:04 +0530

    Size king fixed again

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = '/api'  // Changed to use relative path
 
 function App() {
   const [file, setFile] = useState(null)
